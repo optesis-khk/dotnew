9703fe1d353877fcde5c012eebd320ccf06e5080
Yowit-mbathily <44680721+optesis-khk@users.noreply.github.com>
2021-11-17 13:29:25 +0000

    deltatech_stock_date

---
+++ deltatech_stock_date/wizard/__init__.py
@@ -0,0 +1,7 @@
+# ©  2015-2018 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from . import stock_immediate_transfer
+from . import stock_backorder_confirmation
+from . import stock_quantity_history
